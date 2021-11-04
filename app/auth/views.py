d7a05781555b820cc167da627ee4b4a5b337e1ba
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-04 10:47:25 +0300

    write proper code .

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,8 +36,8 @@ def login():
         flash('Invalid username or Password')
 
     title = "watchlist login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
-
+    return render_template('auth/login.html',login_form=login_form,title=title)
+  
 @auth.route('/logout')
 @login_required
 def logout():
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'mwendaB'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:mwendaB@localhost/watchlist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
